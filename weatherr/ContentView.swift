495153d4bf1e47754e20cd4332ac84a0fb496dad
fatimah <fatimmaah1994@gmail.com>
2022-08-18 18:23:48 -0700

    Initial Commit

---
+++ weatherr/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weatherr
+//
+//  Created by فاطمة on 20/01/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ weatherr/weatherrApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherrApp.swift
+//  weatherr
+//
+//  Created by فاطمة on 20/01/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct weatherrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
